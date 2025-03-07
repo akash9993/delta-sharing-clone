c1d991e501c42371ad33ea627fe604f43b3682b8
akash03 <akash03@nagarro.com>
2025-03-07 19:12:47 +0530

    format

--- server/src/main/scala/io/delta/sharing/server/utils/DatabaseHelper.scala
+++ server/src/main/scala/io/delta/sharing/server/utils/DatabaseHelper.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright (2021) The Delta Lake Project Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.delta.sharing.server.utils
 
 import java.sql.{Connection, DriverManager, PreparedStatement}
@@ -8,7 +24,11 @@ object DatabaseHelper {
   private val logger = LoggerFactory.getLogger(this.getClass)
 
   // Azure SQL Server Connection String
-   private val url = "jdbc:sqlserver://ceer-ods.database.windows.net:1433;database=data-marketplace-poc;user=dbadmin;password=Ceer@123456;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;"
+  private val url =
+  "jdbc:sqlserver://ceer-ods.database.windows.net:1433;" +
+  "database=data-marketplace-poc;user=dbadmin;password=Ceer@123456;" +
+  "encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;" +
+  "loginTimeout=30;"
 
   // Function to insert request info into the database
   def logRequest(requestType: String, requestData: String): Unit = {
